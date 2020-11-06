0e2dd7f4cc8e222d432ce8629d57e8e8b8eee171
TimvanAlphen <55843527+TimvanAlphen@users.noreply.github.com>
2020-11-06 13:14:50 +0100

    Cleaning repo

---
+++ exercise/d3-making-a-face/index.js
@@ -0,0 +1,69 @@
+import {select, arc } from 'd3';
+
+const svg = select('svg');
+const width = +svg.attr('width');
+const height = +svg.attr('height');
+
+const g = svg.append('g')
+  .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+console.log(typeof width);
+
+const circle = g.append('circle')
+  .attr('r', height / 2)
+  .attr('fill', 'yellow')
+  .attr('stroke', 'black')
+  .attr('stroke-width', 5);
+
+const eyeSpacing = 50;
+const eyeYOffset = -50;
+const eyeRadius = 20;
+const eyebrowWidth = 60;
+const eyebrowHeight = 15;
+const eyebrowYOffset = -40;
+
+const eyesG = g
+  .append('g')
+    .attr('transform', `translate(0, ${eyeYOffset})`);
+
+const leftEye = eyesG
+  .append('circle')
+    .attr('r', eyeRadius)
+    .attr('cx', eyeSpacing);
+
+const rightEye = eyesG
+  .append('circle')
+    .attr('r', eyeRadius)
+    .attr('cx', - eyeSpacing);
+
+const eyebrowsG = eyesG
+  .append('g')
+    .attr('transform', `translate(0, ${eyebrowYOffset})`);
+
+eyebrowsG
+  .transition().duration(2000)
+    .attr('transform', `translate(0, ${eyebrowYOffset - 50})`)    
+  .transition().duration(2000)
+    .attr('transform', `translate(0, ${eyebrowYOffset})`);
+
+const leftEyebrow = eyebrowsG
+  .append('rect')
+    .attr('x', -eyeSpacing - eyebrowWidth / 2)
+    .attr('width', eyebrowWidth)
+    .attr('height', eyebrowHeight);
+
+const rightEyebrow = eyebrowsG
+  .append('rect')
+    .attr('x', +eyeSpacing - eyebrowWidth / 2)
+    .attr('width', eyebrowWidth)
+    .attr('height', eyebrowHeight)
+
+
+const mouth = g
+  .append('path')
+    .attr('d', arc()({
+      innerRadius: 0,
+      outerRadius: 60,
+      startAngle: Math.PI / 2,
+      endAngle: Math.PI * 3 / 2
+  }));
